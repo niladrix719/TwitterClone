360f0fd057172750bed47244b39bed5259b34db8
Niladri Adhikary <rickyrickado14@gmail.com>
2023-12-24 01:13:11 +0530

    addd delete functionality

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -14,6 +14,22 @@ class HomeController < ApplicationController
     @posts = Post.all
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to home_path, notice: 'Post was successfully deleted.'
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to home_path, notice: 'Post was successfully updated.'
+    else
+      flash[:alert] = @post.errors.full_messages.join(', ')
+      render :new
+    end
+  end
+
   private
 
   def post_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root "home#index"
   get "/home", to: "home#index"
   post "/home/create", to: "home#create", as: "home_create"
+  delete "/home/:id", to: "home#destroy", as: "home_destroy"
+  patch "/home/:id", to: "home#update", as: "home_update"
 end
